8dc565683d04cb246fad09eeda44683d1df24426
JPR <57785763+openshiftadmin@users.noreply.github.com>
2024-06-24 18:35:41 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,3 +4,5 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
 systemctl start jenkins
+mqbdkjdbq.fdbke
+qwjdbqekljbe
