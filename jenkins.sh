d90c55042c9aa80ea2504f295bb88417a3cd7592
JPR <57785763+openshiftadmin@users.noreply.github.com>
2024-06-24 18:53:02 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,12 +4,3 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
 systemctl start jenkins
-mqbdkjdbq.fdbke
-qwjdbqekljbenclkenlckek
-hey tgis auto source trigger
-hey this ext build
-this is again next build to show demo
-
-hey this final build i am doing here
-
-final build
